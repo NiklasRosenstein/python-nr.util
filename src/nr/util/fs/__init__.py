da3b15353389aa8cb2b31c12d1a0f8673685f368
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-02-11 21:54:13 +0100

    add `nr.util.fs._walk.recurse_directory()`

--- src/nr/util/fs/__init__.py
+++ src/nr/util/fs/__init__.py
@@ -3,9 +3,11 @@
 
 from ._atomic import atomic_swap, atomic_write
 from ._discovery import get_file_in_directory
+from ._walk import recurse_directory
 
 __all__ = [
   'atomic_swap',
   'atomic_write',
   'get_file_in_directory',
+  'recurse_directory',
 ]
