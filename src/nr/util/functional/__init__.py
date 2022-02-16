65e661064c6e068a0a94ad0fc85fba0b44085cdf
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-02-16 22:34:27 +0100

    deprecation: move `nr.util.coalesce()` to `nr.util.functional.coalesce()`, the original function is still available but marked as deprecated

--- src/nr/util/functional/__init__.py
+++ src/nr/util/functional/__init__.py
@@ -1,7 +1,9 @@
 
+from ._assure import assure
+from ._coalesce import coalesce
 from ._consumer import Consumer
 from ._once import Once
 from ._predicate import Predicate
 from ._supplier import Supplier, ContextSupplier
 
-__all__ = ['Consumer', 'Once', 'Predicate', 'Supplier', 'ContextSupplier']
+__all__ = ['assure', 'coalesce', 'Consumer', 'Once', 'Predicate', 'Supplier', 'ContextSupplier']
