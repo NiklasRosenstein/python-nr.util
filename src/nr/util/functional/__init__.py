6e02dd237c4be7bfd5cdc06239f354de744e519e
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-02-22 19:24:20 +0100

    feature: add `nr.util.functional.supplier_get()`

--- src/nr/util/functional/__init__.py
+++ src/nr/util/functional/__init__.py
@@ -4,6 +4,6 @@ from ._coalesce import coalesce
 from ._consumer import Consumer
 from ._once import Once
 from ._predicate import Predicate
-from ._supplier import Supplier, ContextSupplier
+from ._supplier import supplier_get, Supplier, ContextSupplier
 
-__all__ = ['assure', 'coalesce', 'Consumer', 'Once', 'Predicate', 'Supplier', 'ContextSupplier']
+__all__ = ['assure', 'coalesce', 'Consumer', 'Once', 'Predicate', 'supplier_get', 'Supplier', 'ContextSupplier']
