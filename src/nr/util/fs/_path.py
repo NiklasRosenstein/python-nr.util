848295448c81cdbf7df6d53a5dc8b4106c9e453b
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-02-24 02:17:33 +0100

    fix: `is_relative_to()` should also use alternative impl

--- src/nr/util/fs/_path.py
+++ src/nr/util/fs/_path.py
@@ -9,7 +9,7 @@ def is_relative_to(a: Path | str, b: Path | str) -> bool:
   """ Returns `True` if path *a* is relative to path *b*. A backfill for #Path.is_relative_to() for Python versions
   older than 3.9. """
 
-  if sys.version_info < (3, 8):
+  if sys.version_info < (3, 9):
     try:
       Path(a).relative_to(b)
     except ValueError:
