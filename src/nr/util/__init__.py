23edfdfe3b69f706f8d1523e9e726c31306941a4
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-02-25 18:42:28 +0100

    release 0.7.6

--- src/nr/util/__init__.py
+++ src/nr/util/__init__.py
@@ -2,7 +2,7 @@
 """ General purpose utility library. """
 
 __author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
-__version__ = "0.7.5"
+__version__ = "0.7.6"
 
 
 # Backwards compatibility
