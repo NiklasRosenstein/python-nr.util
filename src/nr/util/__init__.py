5083b06a865c9c7507264cf4be23ca93aa35124f
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-04-18 23:12:07 +0200

    release 0.8.9

--- src/nr/util/__init__.py
+++ src/nr/util/__init__.py
@@ -2,7 +2,7 @@
 """ General purpose utility library. """
 
 __author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
-__version__ = "0.8.8"
+__version__ = "0.8.9"
 
 
 from .chaindict import ChainDict
